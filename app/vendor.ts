3ff9d450f346fa67e9de597778be0e71f38c87e6
Will Soto <will.soto9@gmail.com>
2016-03-11 15:22:37 -0500

    feat(app): get typescript and angular2 setup

---
+++ app/vendor.ts
@@ -0,0 +1,8 @@
+// Polyfills
+// fixes for non ES6 browsers (IE, safari, ...)
+// import 'es6-shim/es6-shim';
+// import 'angular2/bundles/angular2-polyfills';
+import 'reflect-metadata';
+
+// RxJS
+import 'rxjs';
--- webpack.config.js
+++ webpack.config.js
@@ -2,51 +2,86 @@ var path = require('path');
 
 var webpack = require('webpack');
 
+// plugins
 var CopyWebpackPlugin = require('copy-webpack-plugin');
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
+
+var NODE_ENV = process.env.NODE_ENV || 'development';
+var isProduction = NODE_ENV === 'production';
 
 module.exports = {
   devtool: 'cheap-module-inline-source-map',
   debug: true,
   context: __dirname + '/app',
   entry: {
-    index: './index.js',
-    vendor: [
-      'lodash'
-    ]
+    app: './app.ts',
+    vendor: './vendor.ts'
   },
   output: {
-    filename: '[name].bundle.js',
     path: __dirname + '/build',
-    publicPath: '/'
+    publicPath: '/',
+    filename: isProduction ? '[name].bundle.[hash].js' : '[name].bundle.js',
+    chunkFilename: isProduction ? '[id].[hash].chunk.js' : '[id].chunk.js'
   },
   module: {
     loaders: [{
-      test: /\.js$/,
-      exclude: [
-        /node_modules/
-      ],
-      loaders: ['babel-loader']
+      test: /\.ts$/,
+      loader: 'ts',
+      query: {
+        'ignoreDiagnostics': [
+          2403, // 2403 -> Subsequent variable declarations
+          2300, // 2300 -> Duplicate identifier
+          2374, // 2374 -> Duplicate number index signature
+          2375, // 2375 -> Duplicate string index signature
+          2502 // 2502 -> Referenced directly or indirectly
+        ]
+      }
     }, {
       test: /\.html$/,
-      loader: 'html-loader'
+      loader: 'html'
     }, {
       test: /\.json$/,
-      loader: 'json-loader'
+      loader: 'json'
     }]
   },
+  noParse: [
+    /.+zone\.js\/dist\/.+/,
+    /.+angular2\/bundles\/.+/,
+    /angular2-polyfills\.js/
+  ],
   resolve: {
     root: path.resolve(__dirname, 'app/'),
-    extenstions: ['', '.js', '.json', '.html']
+    extenstions: ['', '.js', '.json', '.html', '.ts']
   },
   plugins: [
     new webpack.ProvidePlugin({
       'process.env': {
-        'NODE_ENV': process.env.NODE_ENV || 'development'
+        'NODE_ENV': JSON.stringify(NODE_ENV)
       }
     }),
-    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
+    new CommonsChunkPlugin({
+      name: 'vendor',
+      filename: isProduction ? '[name].bundle.[hash].js' : '[name].bundle.js',
+      minChunks: Infinity
+    }),
+    new CommonsChunkPlugin({
+      name: 'common',
+      filename: isProduction ? '[name].bundle.[hash].js' : '[name].bundle.js',
+      minChunks: 2,
+      chunks: ['app', 'vendor']
+    }),
     new CopyWebpackPlugin([{
       from: '*.{html,json}'
-    }])
+    }]),
+    // new webpack.optimize.DedupePlugin(),
+    new BrowserSyncPlugin({
+      server: {
+        baseDir: ['build/'],
+        routes: {
+          '/node_modules': 'node_modules'
+        }
+      }
+    })
   ]
 };
