2c0fdb0a81dee0c4666ad27bbd51132d13f05209
Will Soto <will.soto9@gmail.com>
2016-04-16 21:35:37 -0400

    WIP: initial switch to redux using 'real-world-example' from redux docs
    Signed-off-by: Will Soto <will.soto9@gmail.com>

--- app/app.spec.ts
+++
@@ -1,7 +0,0 @@
-import {expect} from 'chai';
-
-describe('testing', function() {
-  it('should work', function() {
-    expect(true).to.be.true;
-  });
-});
--- webpack.config.js
+++ webpack.config.js
@@ -1,73 +1,35 @@
 var path = require('path');
-
 var webpack = require('webpack');
 
-// plugins
-var CopyWebpackPlugin = require('copy-webpack-plugin');
-var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-
-var NODE_ENV = process.env.NODE_ENV || 'development';
-var isProduction = NODE_ENV === 'production';
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  devtool: 'cheap-module-inline-source-map',
-  debug: true,
-  context: __dirname + '/app',
+  devtool: 'cheap-module-eval-source-map',
+  context: `${__dirname}/app`,
   entry: {
-    app: './app.ts',
-    vendor: './vendor.ts'
+    index: [
+      'webpack-hot-middleware/client',
+      './index.jsx'
+    ]
   },
   output: {
-    path: __dirname + '/build',
-    publicPath: '/',
-    filename: isProduction ? '[name].bundle.[hash].js' : '[name].bundle.js'
-  },
-  module: {
-    loaders: [{
-      test: /\.ts$/,
-      loader: 'ts',
-      query: {
-        'ignoreDiagnostics': [
-          2403, // 2403 -> Subsequent variable declarations
-          2300, // 2300 -> Duplicate identifier
-          2374, // 2374 -> Duplicate number index signature
-          2375, // 2375 -> Duplicate string index signature
-          2502 // 2502 -> Referenced directly or indirectly
-        ]
-      }
-    }, {
-      test: /\.html$/,
-      loader: 'html'
-    }, {
-      test: /\.json$/,
-      loader: 'json'
-    }]
-  },
-  noParse: [
-    /.+zone\.js\/dist\/.+/,
-    /.+angular2\/bundles\/.+/,
-    /angular2-polyfills\.js/
-  ],
-  resolve: {
-    root: path.resolve(__dirname, 'app/'),
-    extenstions: ['', '.js', '.json', '.html', '.ts']
+    path: path.join(__dirname, 'build'),
+    filename: '[name].bundle.js',
+    publicPath: '/'
   },
   plugins: [
-    new webpack.ProvidePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify(NODE_ENV)
-      }
-    }),
-    new CopyWebpackPlugin([{
-      from: '*.{html,json}'
-    }]),
-    new BrowserSyncPlugin({
-      server: {
-        baseDir: ['build/'],
-        routes: {
-          '/node_modules': 'node_modules'
-        }
-      }
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      template: 'index.html'
     })
-  ]
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loaders: ['babel'],
+      exclude: /node_modules/
+    }]
+  }
 };
