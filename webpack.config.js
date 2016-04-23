e1e6742510e35be9d201019e46c9f459cfa2ce79
Will Soto <will.soto9@gmail.com>
2016-04-23 13:00:27 -0400

    refactor(webpack): remove hot module reload
    Also adds css-loader, style-loader, json-loader and code splitting
    
    Signed-off-by: Will Soto <will.soto9@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -2,34 +2,53 @@ var path = require('path');
 var webpack = require('webpack');
 
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var pkg = require('./package.json');
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   context: `${__dirname}/app`,
   entry: {
-    index: [
-      'webpack-hot-middleware/client',
-      './index.jsx'
-    ]
+    index: './index.jsx',
+    // being lazy right now
+    vendor: Object.keys(pkg.dependencies)
   },
   output: {
     path: path.join(__dirname, 'build'),
     filename: '[name].bundle.js',
-    publicPath: '/'
+    publicPath: ''
   },
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({
-      inject: 'body',
-      template: 'index.html'
-    })
-  ],
+  resolve: {
+    extensions: [
+      '',
+      '.js',
+      '.jsx',
+      '.css',
+      '.html',
+      '.json'
+    ]
+  },
   module: {
     loaders: [{
       test: /\.jsx?$/,
       loaders: ['babel'],
       exclude: /node_modules/
+    }, {
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+    }, {
+      test: /\.json/,
+      loader: 'json'
     }]
-  }
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
+    new ExtractTextPlugin('[name].css'),
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      template: 'index.html'
+    })
+  ]
 };
