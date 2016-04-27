b43adde2995bddae7b556e4b4caf0948c5373b4b
Will Soto <will.soto9@gmail.com>
2016-04-27 11:22:12 +0100

    refactor(routes): clean routes
    Signed-off-by: Will Soto <will.soto9@gmail.com>

--- app/routes.jsx
+++ app/routes.jsx
@@ -5,13 +5,16 @@ import App from './containers/App.jsx';
 import Themes from './containers/Themes.jsx';
 
 import Editor from './components/editor/index.jsx';
+import Create from './components/create/index.jsx';
 
 export default (
   <Router history={hashHistory}>
     <Route path="/" component={App}>
-      <IndexRoute component={Themes}/>
-      <Route path="themes" component={Themes}/>
-      <Route path="editor" component={Editor}/>
+      <IndexRoute component={Themes}></IndexRoute>
+
+      <Route path="themes" component={Themes}></Route>
+      <Route path="themes/create" component={Create}></Route>
+      <Route path="editor" component={Editor}></Route>
     </Route>
   </Router>
 );
