63474cabd302a3430255355d8000184f64a55037
Will Soto <will.soto9@gmail.com>
2016-04-28 09:35:26 +0100

    WIP: add CodeMirror
    Signed-off-by: Will Soto <will.soto9@gmail.com>

--- app/components/editor/index.jsx
+++ app/components/editor/index.jsx
@@ -1,10 +1,48 @@
 import React from 'react';
+import CodeMirror from 'react-codemirror';
+
+// languages for codemirror
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/xml/xml';
+import 'codemirror/mode/markdown/markdown';
+
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
 
 export default class Editor extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      code: '// Working'
+    };
+
+    this.options = {
+      lineNumbers: true,
+      mode: 'javascript',
+      readOnly: false,
+      theme: 'material'
+    };
+
+    this.updateCode = this.updateCode.bind(this);
+  }
+
+  updateCode(newCode) {
+    this.setState({
+      code: newCode
+    });
+  }
+
   render() {
     return (
       <div>
-        <h1>Hello from Editor</h1>
+        <CodeMirror
+          ref="editor"
+          value={this.state.code}
+          onChange={this.updateCode}
+          options={this.options}
+          autoSave={true}>
+        </CodeMirror>
       </div>
     );
   }
